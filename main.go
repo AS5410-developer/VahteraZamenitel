de9de4bee4c9d0b16947512af529546e8163c585
AS5410 <aleksandrskripnikov568@gmail.com>
2024-10-16 17:52:37 +0300

    Added base program.

---
+++ main.go
@@ -0,0 +1,13 @@
+// Copyright 2024 AS5410. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("AS Временный Вахтёр &copy 2024")
+}
